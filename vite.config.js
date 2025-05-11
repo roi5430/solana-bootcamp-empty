dee05147950700afdfe501dfe2dc19269326f5a7
kseniia <104863533+roi5430@users.noreply.github.com>
2025-05-11 23:12:25 +0300

    update vite.config.ts

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import path from "path";
-
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { checker } from "vite-plugin-checker";
@@ -7,21 +6,20 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 // import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
-  plugins: [
-    react(),
-    // tailwindcss(),
-    svgr(),
-    nodePolyfills(),
-    tsconfigPaths({ projects: ["./"] }),
-    checker({
-      typescript: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        // tailwindcss(),
+        svgr(),
+        nodePolyfills(),
+        tsconfigPaths({ projects: ["./"] }),
+        checker({
+            typescript: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
